a4525b0e2aaa15ce09e6d6f7c30844421ad564f7
Phi Hoang <ntphoang205@gmail.com>
2026-05-05 08:55:30 +0700

    add: .env

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import router from '../routes';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8002',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
